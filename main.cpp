02c5a7606de41bb11719c112d51630638160d121
shubhambhatt-07 <159576279+shubhambhatt-07@users.noreply.github.com>
2024-02-10 00:35:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define SPEED ios::sync_with_stdio(false); cin.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+int32_t main() {
+
+	SPEED
+	
+	
+}
